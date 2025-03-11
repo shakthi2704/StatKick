018377b6a05756622a4db1747f583ded2c514aa6
Satheesh <shakthi2704@gmail.com>
2025-03-11 19:14:28 +0530

    setting section

--- components/main-nav/main-nav.jsx
+++ components/main-nav/main-nav.jsx
@@ -9,23 +9,22 @@ import {
   FaChartBar,
   FaNewspaper,
   FaFlag,
+  FaUsers,
+  FaStar,
 } from "react-icons/fa"
-import { RiTeamFill } from "react-icons/ri"
-import { MdDashboard, MdFavorite } from "react-icons/md"
-import { GiTeamDowngrade } from "react-icons/gi"
 
 const navLinks = [
-  { href: "/", label: "Home", icon: <MdDashboard size={12} /> },
+  { href: "/", label: "Home", icon: <FaHome size={12} /> },
   { href: "/league", label: "League", icon: <FaTable size={12} /> },
-  { href: "/teams", label: "Teams", icon: <FaFlag size={12} /> },
-  { href: "/players", label: "Players", icon: <RiTeamFill size={12} /> },
+  { href: "/teams", label: "Teams", icon: <FaUsers size={12} /> },
+  { href: "/players", label: "Players", icon: <FaFlag size={12} /> },
   {
     href: "/stats",
     label: "Stats & Analytics",
-    icon: <FaChartBar size={12} />,
+    icon: <FaChartBar size={12} />, // Stats icon
   },
   { href: "/news", label: "News", icon: <FaNewspaper size={12} /> },
-  { href: "/favorites", label: "Favorites", icon: <MdFavorite size={12} /> },
+  { href: "/favorites", label: "Favorites", icon: <FaStar size={12} /> },
 ]
 
 const MainNav = () => {
@@ -39,12 +38,10 @@ const MainNav = () => {
   if (!isClient) return null
 
   return (
-    <nav className=" text-foreground p-4 text-xs">
+    <nav className=" text-foreground p-4 text-sm">
       <ul className="flex space-x-4">
         {navLinks.map((link, index) => (
           <li key={link.href + index}>
-            {" "}
-            {/* Use the combination of href and index for unique key */}
             <Link
               href={link.href}
               className={`flex items-center space-x-2 ${
